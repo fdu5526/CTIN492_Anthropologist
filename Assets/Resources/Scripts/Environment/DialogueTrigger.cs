7cf09eb59527ee222624a311efe4cb82a9c8d0d7
Dave Yan <fdu5526@live.com>
2016-04-11 20:55:29 -0700

    dialogue trigger added

---
+++ Assets/Resources/Scripts/Environment/DialogueTrigger.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DialogueTrigger : MonoBehaviour {
+	DialogueBox dialogueBox;
+
+	// Use this for initialization
+	void Start () {
+		dialogueBox = GameObject.Find("Canvas/DialogueBox").GetComponent<DialogueBox>();
+	}
+
+
+	void OnTriggerStay2D (Collider2D other) {
+		dialogueBox.Activate(true);
+	}
+
+	void OnTriggerExit2D (Collider2D other) {
+		dialogueBox.Activate(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
