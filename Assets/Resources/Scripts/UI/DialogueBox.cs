669c93584f7c04e0cfb7700d54b2ccaf5eabb8c7
Dave Yan <fdu5526@live.com>
2016-04-11 20:43:22 -0700

    added dialogue box

---
+++ Assets/Resources/Scripts/UI/DialogueBox.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class DialogueBox : MonoBehaviour {
+
+	Text dialogueText;
+
+	// Use this for initialization
+	void Start () {
+		Activate(false);
+	}
+
+
+	public void Activate (bool activate) {
+		GetComponent<CanvasGroup>().alpha = activate ? 1f : 0f;
+	}
+
+
+	public void SetText (string text) {
+		dialogueText.text = text;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
