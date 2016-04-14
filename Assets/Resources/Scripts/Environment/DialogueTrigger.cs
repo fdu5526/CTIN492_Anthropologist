5405519c4e2f55d4692f1344d33539c85233a2cc
Dave Yan <fdu5526@live.com>
2016-04-14 00:08:47 -0700

    have 2 story bits yay

--- Assets/Resources/Scripts/Environment/DialogueTrigger.cs
+++ Assets/Resources/Scripts/Environment/DialogueTrigger.cs
@@ -19,7 +19,7 @@ public class DialogueTrigger : MonoBehaviour {
 
 
 	void OnTriggerStay2D (Collider2D other) {
-		if (Input.GetKeyDown("e")) {
+		if (Input.GetKey("e")) {
 			dialogueBox.Activate(true);
 			dialogueBox.SetText(text);
 			GetComponent<Collider2D>().enabled = false;
