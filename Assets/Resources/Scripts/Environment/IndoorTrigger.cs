9afd9d875315c10983d0075ef2bbe64dbab19411
Dave Yan <fdu5526@live.com>
2016-04-13 20:39:16 -0700

    ambient sounds in and out

--- Assets/Resources/Scripts/Environment/IndoorTrigger.cs
+++ Assets/Resources/Scripts/Environment/IndoorTrigger.cs
@@ -6,15 +6,18 @@ public class IndoorTrigger : MonoBehaviour {
 	bool transitionToIndoor;
 
 	Player player;
+	MainCamera mainCamera;
 
 	// Use this for initialization
 	void Start () {
 		player = GameObject.Find("Player").GetComponent<Player>();
+		mainCamera = GameObject.Find("Main Camera").GetComponent<MainCamera>();
 	}
 
 
 	void OnTriggerExit2D (Collider2D other) {
 		player.isIndoors = transitionToIndoor;
+		mainCamera.SwitchIndoors(transitionToIndoor);
 	}
 	
 	// Update is called once per frame
--- Assets/Resources/Scripts/Player/MainCamera.cs
+++ Assets/Resources/Scripts/Player/MainCamera.cs
@@ -4,10 +4,24 @@ using System.Collections;
 public class MainCamera : MonoBehaviour {
 
 	GameObject player;
+	AudioClip[] ambiences;
 
 	// Use this 	for initialization
 	void Start () {
 		player = GameObject.Find("Player").gameObject;
+		ambiences = new AudioClip[2];
+		ambiences[0] = Resources.Load<AudioClip>("Sounds/Environment/windLoop");
+		ambiences[1] = Resources.Load<AudioClip>("Sounds/Environment/hum");
+	}
+
+
+	public void SwitchIndoors (bool isInDoors) {
+		if (isInDoors) {
+			GetComponent<AudioSource>().clip = ambiences[1];
+		} else {
+			GetComponent<AudioSource>().clip = ambiences[0];
+		}
+		GetComponent<AudioSource>().Play();
 	}
 	
 	// Update is called once per frame
