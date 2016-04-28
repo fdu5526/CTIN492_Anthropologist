bf9518dff944fc3b898f63e49a6ce1967fae6ff3
Shiwei Yan <shiweiya@sca.usc.edu>
2016-04-27 20:11:15 -0700

    chairs

---
+++ Assets/Resources/scripts/Environment/RandomStartRotation.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomStartRotation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		float randZ = UnityEngine.Random.Range(-40f, 40f);
+		Vector3 r = transform.eulerAngles;
+
+		transform.eulerAngles = new Vector3(r.x, r.y, r.z + randZ);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
